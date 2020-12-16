790cd6cb554cd5157f758fcf4f4043ec4812d5ed
DaehyeokYou <ydh4085@naver.com>
2020-12-16 14:00:03 +0900

    fix context nullpointerexception error

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -14,7 +14,7 @@ public class DemoApplication {
 	public static ApplicationContext applicationContext;
     
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		applicationContext = SpringApplication.run(DemoApplication.class, args);
 	}
 	
 
